5d7d6fb845956cc80609427210f23d4eff6e0791
LLIypKa <shilkoaa@gmail.com>
2024-06-06 23:52:46 +0700

    fixed adapter

--- app/src/main/java/com/example/lab13/Classes/OrdersModel/OrdersAdapter.java
+++ app/src/main/java/com/example/lab13/Classes/OrdersModel/OrdersAdapter.java
@@ -42,7 +42,7 @@ public class OrdersAdapter extends ArrayAdapter<Order> {
 
         Cursor metalCursor = db.query(DBHelper.TABLE_METAL_STRUCTURES, null, null, null, null, null, null);
         int metalColumnIndex = metalCursor.getColumnIndex(DBHelper.METAL_STRUCTURES_NAME);
-        metalCursor.move(order.MetalID + 1);
+        metalCursor.move(order.MetalID);
 
         TextView textViewOrderID = convertView.findViewById(R.id.textViewOrderID);
         TextView textViewClientID = convertView.findViewById(R.id.textViewClientID);

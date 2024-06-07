4c8e61ee8d8cc10da0dacd52f0bcb80729331af7
LLIypKa <shilkoaa@gmail.com>
2024-06-07 23:22:08 +0700

    fixed metals

--- app/src/main/java/com/example/lab13/Classes/SuppliesModel/PutSuppliesToDB.java
+++ app/src/main/java/com/example/lab13/Classes/SuppliesModel/PutSuppliesToDB.java
@@ -10,10 +10,10 @@ public class PutSuppliesToDB {
 
     public static List<Supplies> GetSupplies() {
         if(Supplies.isEmpty()) {
-            Supplies.add(new Supplies(0, 0, 1, 200, LocalDate.now().plus(1, ChronoUnit.DAYS)));
-            Supplies.add(new Supplies(1, 1, 2, 200, LocalDate.now().plus(2, ChronoUnit.DAYS)));
-            Supplies.add(new Supplies(2, 0, 3, 200, LocalDate.now().plus(3, ChronoUnit.DAYS)));
-            Supplies.add(new Supplies(3, 1, 4, 200, LocalDate.now().plus(4, ChronoUnit.DAYS)));
+            Supplies.add(new Supplies(0, 0, 0, 200, LocalDate.now().plus(1, ChronoUnit.DAYS)));
+            Supplies.add(new Supplies(1, 1, 1, 200, LocalDate.now().plus(2, ChronoUnit.DAYS)));
+            Supplies.add(new Supplies(2, 0, 2, 200, LocalDate.now().plus(3, ChronoUnit.DAYS)));
+            Supplies.add(new Supplies(3, 1, 3, 200, LocalDate.now().plus(4, ChronoUnit.DAYS)));
         }
 
         return Supplies;
